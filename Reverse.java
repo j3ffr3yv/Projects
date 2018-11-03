1ce44ee54b7619144ca5346df1aa75db92567b2b
j3ffr3yv <44720313+j3ffr3yv@users.noreply.github.com>
2018-11-03 16:51:50 -0700

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,34 @@
+//Written by:  Jeffrey Viramontes and Mario Garcia
+//Assignment:  7.2
+//Class:       Co Sci 209
+//Date:        5/9/17
+//Description: Reverses the numbers entered.
+
+public class Reverse
+{
+   public static void main(String[]args)
+   {
+      //Create Scanner
+      java.util.Scanner input = new java.util.Scanner(System.in);
+      
+      //Description to user
+      System.out.println("This program collects 10 numbers and displays in reverse. ");
+      
+      //Array for numbers entered
+      int[] numbers=new int[10];
+      
+      //Instruction to user
+      System.out.print("Enter 10 numbers: ");
+      
+      //loop for user input
+      for(int i=0;i<numbers.length;i++)
+      {
+         numbers[i]=input.nextInt();
+      }
+         //loop to display the numbers in reverse
+         for (int i = numbers.length - 1; i >= 0; i--) 
+         {
+            System.out.print(numbers[i] + " ");
+         }
+   }
+}
